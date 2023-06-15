550805baf2f55eff373b8f4e9e757e6eb6b25c55
Én <118002424+Hoagyen1102@users.noreply.github.com>
2023-06-15 22:12:19 +0700

    Update connect.php

--- connect.php
+++ connect.php
@@ -2,7 +2,7 @@
 $localhost = "four.crj5jnssjjr0.us-east-1.rds.amazonaws.com";
 $username = "Four";
 $password = "dtdmnhom4";
-$db = "four";
+$db = "Four";
 $conn = new PDO("mysql:host=$localhost;dbname=$db,charset=utf8", $username, $password);
 
 ?>
